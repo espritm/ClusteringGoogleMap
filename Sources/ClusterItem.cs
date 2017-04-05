b1f3c1939822daaf549a5176a77566aba41ff3a6
Maxime Esprit <maxime.esprit@gmail.com>
2017-04-05 14:57:40 +0200

    Création projet + première partie : affichage simple de marker + cluster.

---
+++ Sources/ClusterItem.cs
@@ -0,0 +1,25 @@
+﻿using Android.Gms.Maps.Model;
+using Com.Google.Maps.Android.Clustering;
+using System;
+
+namespace ClusteringGoogleMap
+{
+    public class ClusterItem : Java.Lang.Object, IClusterItem
+    {
+        public LatLng Position { get; set; }
+        public string Snippet { get; set; }
+        public string Title { get; set; }
+
+
+        public ClusterItem(double lat, double lon)
+        {
+            Position = new LatLng(lat, lon);
+            Title = lat.ToString() + ", " + lon.ToString();
+        }
+
+        public ClusterItem(IntPtr handle, Android.Runtime.JniHandleOwnership transfer)
+        : base(handle, transfer)
+        {//To avoid Leaky abstraction.... http://stackoverflow.com/questions/10593022/monodroid-error-when-calling-constructor-of-custom-view-twodscrollview/10603714#10603714
+        }
+    }
+}
---
+++ Sources/MainActivity.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using Android.App;
+using Android.OS;
+using Android.Gms.Maps;
+using Com.Google.Maps.Android.Clustering;
+using Android.Gms.Maps.Model;
+using System.Collections.Generic;
+using Android.Support.V7.App;
+
+namespace ClusteringGoogleMap
+{
+    [Activity(Label = "@string/ApplicationName", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : AppCompatActivity, IOnMapReadyCallback
+    {
+        private GoogleMap m_map;
+        private MapView m_mapView;
+        private ClusterManager m_ClusterManager;
+        
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+            m_mapView = FindViewById<MapView>(Resource.Id.mainactivity_mapView);
+
+            m_mapView.OnCreate(bundle);
+            m_mapView.GetMapAsync(this);
+        }
+
+        public void OnMapReady(GoogleMap map)
+        {
+            m_map = map;
+
+            m_ClusterManager = new ClusterManager(this, m_map);
+            m_map.SetOnCameraIdleListener(m_ClusterManager);
+            m_map.SetOnMarkerClickListener(m_ClusterManager);
+            m_map.SetOnInfoWindowClickListener(m_ClusterManager);
+
+            SetupMap();
+        }
+
+        private void SetupMap()
+        {
+            //Show Grenoble on the map
+            LatLng LatLonGrenoble = new LatLng(45.188529, 5.724523);
+            m_map.MoveCamera(CameraUpdateFactory.NewLatLngZoom(LatLonGrenoble, Resources.GetInteger(Resource.Integer.default_zoom_level_googlemaps)));
+
+            List<ClusterItem> lsMarkers = new List<ClusterItem>();
+
+            //Add 50 markers using a spiral algorithm
+            for (int i = 0; i < 50; ++i)
+            {
+                double t = i * Math.PI * 0.33f;
+                double r = 0.005 * Math.Exp(0.1 * t);
+                double x = r * Math.Cos(t);
+                double y = r * Math.Sin(t);
+                ClusterItem newMarker = new ClusterItem(LatLonGrenoble.Latitude + x, LatLonGrenoble.Longitude + y);
+                lsMarkers.Add(newMarker);
+            }
+
+            m_ClusterManager.AddItems(lsMarkers);
+        }
+
+        protected override void OnPause()
+        {
+            base.OnPause();
+            m_mapView.OnPause();
+        }
+
+        protected override void OnResume()
+        {
+            base.OnResume();
+            m_mapView.OnResume();
+        }
+
+        protected override void OnSaveInstanceState(Bundle outState)
+        {
+            base.OnSaveInstanceState(outState);
+            m_mapView.OnSaveInstanceState(outState);
+        }
+    }
+}
+
